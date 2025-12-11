47410ec6baafb1a757ec00adc67e653b386a276b
Enrique Castillo <henrique.castillob@gmail.com>
2025-12-10 19:47:08 -0600

    Se creo un archivo para colocar los recursos de mi servidor

---
+++ recursos.py
@@ -0,0 +1,23 @@
+# Los recursos que mi servidor tendra disponible
+from flask_restful import Resource
+from flask import make_response, render_template
+
+# Vamos a crear Recursos - Para nosotros poder crear recursos
+# lo haremos a través de clases y objetos
+class HolaMundo(Resource):
+    # Los recursos van a poder ejecutar dos acciones -> métodos
+    def get(self):
+        return {'hola': 'hola mundo'}
+
+# Será un recurso
+class PantallaInicio(Resource):
+    def get(self):
+        # renderizamos el cobtenido del html
+        contenido = render_template('index.html')
+        # Retornamos el contenido renderizado en una respuesta
+        return make_response(contenido)
+
+class Despedida(Resource):
+    def get(self):
+        print('Esto es un texto de prueba')
+        return 'Adios'
